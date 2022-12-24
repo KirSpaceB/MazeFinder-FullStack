b67e16313ca42c117624b0b91b81c4b053643fe9
KirSpaceB <productivekirk@gmail.com>
2022-12-24 15:45:28 -0800

    Trying something new, just it case it goes bad

--- Maze/Grid.js
+++ Maze/Grid.js
@@ -23,39 +23,42 @@ export class Grid {
       ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
       ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
       ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['s','n','n','g','n','n','n','n','n','n','n','n','n','n','n','n'],
+      ['n','n','n','n','n','g','n','n','n','n','n','n','n','n','n','n'],
+      ['s','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
     ]
 
     
     this.grid = maze;
 
-    const SQUARE_WRAPPER = document.getElementById('DIV_WRAPPER');
+    const DIV_WRAPPER = document.getElementById('DIV_WRAPPER');
 
-    for(let r = 0; r < maze.length; r++) {
-      for(let c = 0; c < maze[r].length; c++) {
-        
-        if(maze[r][c] === 's') {
-          const START_SQUARE = new GridSquare('div','START_SQUARE');
-          START_SQUARE.setType('start');
-          SQUARE_WRAPPER.appendChild(START_SQUARE.gridSquareElement); 
-        } else if(maze[r][c] === 'g') {
-          const GOAL_SQUARE = new GridSquare('div','START_SQUARE');
-          GOAL_SQUARE.setType('goal');
-          SQUARE_WRAPPER.appendChild(GOAL_SQUARE.gridSquareElement);
-        } else if(maze[r][c] === "n") {
-          const NOTHING_SQUARE = new GridSquare('div','NOTHING_SQUARE');
-          NOTHING_SQUARE.setType('nothing');
-          SQUARE_WRAPPER.appendChild(NOTHING_SQUARE.gridSquareElement)
+
+    for (let r = 0; r < this.grid.length; r++) {
+      for(let c = 0; c < this.grid[r].length; c++) {
+        if(this.grid[r][c] === "s") {
+          const start = new GridSquare('div','start');
+          start.setType('start');
+          DIV_WRAPPER.appendChild(start.gridSquareElement);
+        } else if(this.grid[r][c] === "n") {
+          const nothing = new GridSquare('div','NOTHING_SQUARE');
+          nothing.setType('nothing');
+          this.nothing = nothing;
+          DIV_WRAPPER.appendChild(this.nothing.gridSquareElement);
+        } else if(this.grid[r][c] === "g") {
+          const goal = new GridSquare('div');
+          goal.setType('goal');
+          DIV_WRAPPER.appendChild(goal.gridSquareElement);
         }
       }
     }
+
   }
 
   dfsImplementation() {
     let startRow = 0;
     let startCol = 0;
 
+    // marks the starting point
     for (let r = 0; r < this.grid.length; r++) {
       for(let c = 0; c < this.grid[r].length; c++) {
         if(this.grid[r][c] === "s") {
@@ -65,33 +68,39 @@ export class Grid {
         }
       }
     }
-    
+    // this is what makes it dfs
     const stack = [[startRow,startCol]];
 
     const visited = new Set();
 
     while(stack.length > 0) {
-
+      // output sequence and current node
       const [row,col] = stack.pop();
-
+      // marks visited
       visited.add(`${row},${col}`);
-      // terminates function if "g" is found
-      if(this.grid[row][col] === 'g') {
-        console.log(`Found the goal at row ${row}, colmn ${col}`);
+      // condition to determine if goal exist
+      if (this.grid[row][col] === "g") {
+        console.log('goal found');
         return true
       }
-
+      // checks neighbors
       for(const [dRow, dCol] of [[-1,0],[1,0],[0,-1],[0,1]]) {
         const newRow = row + dRow;
         const newCol = col + dCol;
-        // checks unvisted nodes of curren
+        // marks the bounderies of the grid and checks for new nodes to visit
         if(newRow >= 0 && newRow < this.grid.length && newCol >= 0 && newCol < this.grid.length && !visited.has(`${newRow},${newCol}`)) {
           stack.push([newRow,newCol]);
-          console.log(`Exploring node at row ${newRow}, column ${newCol}`);
+          console.log(`Exploring node at row ${newRow}, column ${newCol}`); 
+          // suppose to change the nothing square to path type to show the path that dfs search is taking
+          setTimeout(() => {
+            console.log(this.grid[newRow][newCol])
+            if(this.grid[newRow][newCol] === "n") {
+              this.nothing.setType('path')
+            }
+          },500);
         }
       }
     }
     console.log('goal not found');
-    return false
   }
 }
