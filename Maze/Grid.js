5ae66ee38e97efe0bc8a1883dca661913d1fac70
KirSpaceB <productivekirk@gmail.com>
2022-12-27 21:01:44 -0800

    //Todo

--- Maze/Grid.js
+++ Maze/Grid.js
@@ -5,27 +5,42 @@ export class Grid {
   constructor() {
     this.maze();
     this.dfsImplementation();
-    this.appendingMaze();
   }
 
   maze() {
+    // create GridSquare objects to occupy the maze
+    const s = new GridSquare('div', 'START_SQUARE');
+    const n = new GridSquare('div', 'NOTHING_SQUARE');
+    const p = new GridSquare('div', 'PATH_SQUARE');
+    const g = new GridSquare('div', 'GOAL_SQUARE');
+    // setting GridSquare object types
+    s.setType('start');
+    n.setType('nothing');
+    p.setType('path');
+    g.setType('goal');
+
+    this.s = s;
+    this.n = n;
+    this.p = p;
+    this.g = g;
+    
     let maze = [
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','g'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['n','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
-      ['s','n','n','n','n','n','n','n','n','n','n','n','n','n','n','n'],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,g],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
+      [s,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n],
     ];
     this.grid = maze;
   }
@@ -33,11 +48,13 @@ export class Grid {
   dfsImplementation() {
     let startRow = 0;
     let startCol = 0;
+    // get the divwrapper DOM element
+    const DIV_WRAPPER = document.getElementById('DIV_WRAPPER');
 
     // marks the starting point
     for (let r = 0; r < this.grid.length; r++) {
       for(let c = 0; c < this.grid[r].length; c++) {
-        if(this.grid[r][c] === "s") {
+        if(this.grid[r][c] === this.s) {
           startRow = r;
           startCol = c;
           break;
@@ -56,16 +73,26 @@ export class Grid {
       // marks visited
       visited.add(`${row},${col}`);
       // condition to determine if goal exist
-      if (this.grid[row][col] === "g") {
+      if (this.grid[row][col] === this.g) {
         console.log('goal found');
         return true
       }
 
-      // So the idea is we change the "n" traversed to p and THEN do the method that appends elements to the grid
-      // logic here is probably wrong fix later
-      if(this.grid[row][col] === "n") {
-        this.grid[row][col] = "p"
+      if(this.grid[row][col] === this.n) {
+        this.grid[row][col] = this.p;
       }
+      
+      // create a conditional that appends GridSquare elements on the grid based on strings on the grid
+      // Double for loop that iterates through the 2D array and appends to div wrapper the element on the index.
+      this.grid.forEach((row) => {
+        row.forEach((col) => {
+          if(col === this.s) {
+            DIV_WRAPPER.appendChild(this.s.gridSquareElement)
+          } else if (col === this.p) {
+            DIV_WRAPPER.appendChild(this.p.gridSquareElement)
+          }
+        })
+      })
 
       // checks neighbors
       for(const [dRow, dCol] of [[-1,0],[1,0],[0,-1],[0,1]]) {
@@ -77,7 +104,7 @@ export class Grid {
         const isNotVisited = !visited.has(`${newRow},${newCol}`);
         if (isValidRow && isValidCol && isNotVisited) {
           stack.push([newRow,newCol]);
-          console.log(`Exploring node at row ${newRow}, column ${newCol}`);
+          // console.log(`Exploring node at row ${newRow}, column ${newCol}`);
         }
       }
     }
@@ -85,7 +112,6 @@ export class Grid {
   }
   // instead of using dfs to append it appends after dfs is used(Problem)
   appendingMaze() {
-    console.log(this.grid);
     const DIV_WRAPPER = document.getElementById('DIV_WRAPPER');
 
     for(let r = 0; r < this.grid.length; r++) {
