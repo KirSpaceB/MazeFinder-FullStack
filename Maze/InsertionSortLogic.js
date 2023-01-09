790a90e88e9d1bd4d9e971226f628adc423d1ca0
KirSpaceB <productivekirk@gmail.com>
2023-01-09 11:13:58 -0800

    Todo: Figure out how to configure maze coordinates to that of insertion sort

--- Maze/InsertionSortLogic.js
+++ Maze/InsertionSortLogic.js
@@ -2,14 +2,14 @@ import { GravityButtonLogic } from "./GravityButtonLogic.js";
 
 export class InsertionSortLogic {
   constructor() {
-    this.sortLogic()
+    this.sortLogic();
   }
 
   async sortLogic() {
     let gridAfterGravityButtonIsClicked = new GravityButtonLogic();
 
     let maze = await gridAfterGravityButtonIsClicked.logic();
-
+    
     const sortButton = document.querySelector('.InsertionSort')
     console.log(sortButton)
 
@@ -17,38 +17,38 @@ export class InsertionSortLogic {
       sortButton.addEventListener('click',resolve)
     });
     
-    this.performtheSort(maze);
+    this.getHighestWallSquareCoordinate(maze);
 
   }
 
-  async performtheSort(maze) {
-    //Initial an array to pass to Insertion Sort as an argument
+  async getHighestWallSquareCoordinate(maze) {
+    // Initialize an array to hold the coordinates of the Wall squares
     let coordinates = [];
-
-    // Iterate through the grid and add a conditional that looks for the highest div that contains a "Wall" class if so push to a seperate array
+    // Get all coordinates of Wall squares on the div
     for(let r = 0; r < maze.length; r++) {
       for(let c = 0; c < maze.length; c++) {
-        if(r > 0 && maze[r - 1][c].style.backgroundColor === "white" && maze[r][c].classList.contains('Wall')) {
-          coordinates.push([r,c])
+        if(maze[r][c].classList.contains('Wall')) {
+          coordinates.push([r,c]);
         }
       }
     }
-    console.log("coordinates in the performtheSort function", coordinates);
-    console.log("maze in the performTheSort function", maze)
+  
     
-  }
-
-  sortCoordinates(coordinates) {
-    for(let i = 1; i < coordinates.length; i++) {
+    // Insertion sort the coordinates array in ascending order based on the second element of each coordinate
+    for (let i = 1; i < coordinates.length; i++) {
+      // Check if this is read
+      console.log('test');
+      // variable 
+      let current = coordinates[i];
       let j = i - 1;
-      let temp = coordinates[i];
-      while(j >= 0 && coordinates[j][0] > temp[0]) {
+      while (j >= 0 && coordinates[j][1] > current[1]) {
         coordinates[j + 1] = coordinates[j];
         j--;
       }
-      coordinates[j + 1] = temp;
+      coordinates[j + 1] = current;
     }
-    return coordinates;
+    console.log(coordinates)
+
   }
 }
 
