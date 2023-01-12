ab25a0ca0e7d94f75e375c1cd1ce2dd7ce5d99cc
KirSpaceB <productivekirk@gmail.com>
2023-01-12 15:24:25 -0800

    Updated

--- Maze/InsertionSortLogic.js
+++ Maze/InsertionSortLogic.js
@@ -21,37 +21,34 @@ export class InsertionSortLogic {
   }
 
   async getCoordinates(maze) {
-    let firstWallCords = [];
-    let secondWallCords = [];
-    // Get all coordinates of divs with class "Wall"
-    for(let r = 0; r < maze.length; r++) {
-      for(let c = 0; c < maze[r].length; c++) {
-        if(r < maze.length && c <= 0 && maze[r][c].classList.contains('Wall')) {
-          firstWallCords.push([r,c])
-        } else if(r < maze.length  && c === 1 && maze[r][c].classList.contains('Wall')) {
-          secondWallCords.push([r,c])
+    let columnsArray = [];
+    for(let c = 0; c < maze[0].length; c++) {
+      let arrayForCols = []
+      for(let r = 0; r < maze.length; r++) {
+        if(maze[r][c].classList.contains('Wall')) {
+          arrayForCols.push(maze[r][c])
         }
       }
-    };
-
-
-    if(firstWallCords.length > secondWallCords.length) {
-      [firstWallCords, secondWallCords] = [secondWallCords, firstWallCords]
-    } else {
-      console.log('First set of coordinates in not greater')
+      columnsArray.push(arrayForCols);
     }
-  
-    for(let i = 0; i < firstWallCords.length; i++) {
-      for(let j = 0; j < firstWallCords[i].length; j++) {
-        maze[firstWallCords[i][0]][firstWallCords[j][1] - 1].style.backgroundColor = 'orange'
+    for(let i = 1; i < columnsArray.length; i++) {
+      let j = i - 1;
+      let temp = columnsArray[i];
+      while(j >= 0 && columnsArray[j].length > temp.length) {
+        columnsArray[j + 1] = columnsArray[j];
+        j--;
       }
+      columnsArray[j + 1] = temp;
     }
 
-    for(let i = 0; i < secondWallCords.length; i++) {
-      for(let j = 0; j < secondWallCords[i].length; j++) {
-        maze[secondWallCords[i][0]][secondWallCords[j][1] + 1].style.backgroundColor = 'red'
-      }
+    let walls = document.getElementsByClassName("Wall");
+    while (walls[0]) {
+      walls[0].parentNode.removeChild(walls[0]);
     }
+
+    console.log(columnsArray)
+
+
 
   }
 }
